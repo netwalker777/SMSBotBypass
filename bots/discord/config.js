2bd74e4fab897ed8ee08338b92891505ebbf86ab
netwalker <100284636+netwalker777@users.noreply.github.com>
2024-12-24 12:35:12 +0300

    Update config.js

--- bots/discord/config.js
+++ bots/discord/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    apiurl: 'http://localhost:1337',
-    apipassword: '',
+    apiurl: 'http://41.90.66.17:1337',
+    apipassword: 'passwordtochange',
 
-    discordtoken: '',
+    discordtoken: 'MTI3NDk5NDI5MTQ4MzM0OTEzMg.G_SjZg.XVxKE-l-vBtDyVMkc31Xe4SYsSFW-avLNIHbCs',
     discordprefix: '!',
     secretpassword: 'ChAnGeThIsWiThYoUrPaSsWoRdNoW!',
     botuser_rolename: 'Bot User',
